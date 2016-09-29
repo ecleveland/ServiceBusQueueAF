7540bdf30ddc72eec01d78855312c198dc675d17
Eric Cleveland <Eric Cleveland@DESKTOP-0IOB8ET>
2016-09-29 09:39:48 -0500

    Add project files.

---
+++ ServiceBusQueueAF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusQueueAF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
