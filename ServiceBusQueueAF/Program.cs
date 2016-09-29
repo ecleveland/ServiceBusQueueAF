a6c04fc134d0c9f402c4fe1d7c93ffdf4d33b245
Eric Cleveland <eric.cleveland9@live.com>
2016-09-29 16:00:28 -0500

    Added topics code.

--- ServiceBusQueueAF/Program.cs
+++ ServiceBusQueueAF/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
 using System;
 
 namespace ServiceBusQueueAF
@@ -13,14 +14,24 @@ namespace ServiceBusQueueAF
 
             var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
 
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists(queueName))
+            {
+                namespaceManager.CreateQueue(queueName);
+            }
+
             var testMessage = "";
 
+            Console.WriteLine("Type \"exit\" to escape program.");
             do
             {
+                Console.Write("Enter a message to send to the queue: ");
                 testMessage = Console.ReadLine();
                 var message = new BrokeredMessage(testMessage);
-                client.Send(message);
-            } while (testMessage.Equals("exit"));
+                if(!testMessage.Equals("exit"))
+                    client.Send(message);
+            } while (!testMessage.Equals("exit"));
         }
     }
 }
---
+++ ServiceBusTopicAF/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusTopicAF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topicName = "orders";
+            string subscription1 = "StandardOrder";
+            string subscription2 = "PermiumOrder";
+
+            var namespaceManager = NamespaceManager.Create();
+
+            if (!namespaceManager.TopicExists(topicName))
+            {
+                var topicDescription = namespaceManager.CreateTopic(topicName);
+            }
+
+            if(!namespaceManager.SubscriptionExists(topicName, subscription1))
+            {
+                var filter = new SqlFilter("Total < 1000");
+                namespaceManager.CreateSubscription(topicName, subscription1, filter);
+            }
+
+            if(!namespaceManager.SubscriptionExists(topicName, subscription2))
+            {
+                var filter = new SqlFilter("Total >= 1000");
+                namespaceManager.CreateSubscription(topicName, subscription2, filter);
+            }
+
+            var topicClient = TopicClient.Create(topicName);
+            var message = new BrokeredMessage("Some Standard Order");
+            message.Properties["Total"] = 100;
+            
+            var message2 = new BrokeredMessage("Some Priority Order");
+            message2.Properties["Total"] = 2000;
+            message2.TimeToLive = new TimeSpan(0, 0, 10);
+
+            topicClient.Send(message);
+            topicClient.Send(message2);
+
+            topicClient.Close();
+        }
+    }
+}
